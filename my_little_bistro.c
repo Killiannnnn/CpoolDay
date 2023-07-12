46ee2a4a72d8c32745d223566fb3eca0f126e7b1
Killiannnnn <113891378+Killiannnnn@users.noreply.github.com>
2023-07-12 12:02:02 +0000

    [ADD] my_little_bistro

---
+++ my_little_bistro.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int my_little_bistro(int value1, char op, int value2) {
+
+    if(op == '+')
+        return value1 + value2;
+    if(op == '-')
+        return value1 - value2;
+    if(op == '/')
+        if(value2 == 0)
+            return 0;
+        return value1 / value2;
+    if(op == '*')
+        return value1 * value2;
+    if(op == '%')
+        return value1 % value2;
+    return 0;
+
+}
