7207222be733636469191ed012abfb9e7222fd81
Killiannnnn <113891378+Killiannnnn@users.noreply.github.com>
2023-07-12 12:38:53 +0000

    [ADD] my_char_replace

---
+++ my_char_replace.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void print_char(char c);
+
+int my_strlen(char* str) {
+
+    int a = 0;
+
+    while(str[a] != '\0')
+        a++;
+    
+    return a;
+
+}
+
+char* my_replace_char(char* origin, char toFind, char toReplace) {
+
+    int len = my_strlen(origin);
+
+    for(int i = 0; i < len; i++)
+        if(origin[i] == toFind)
+            origin[i] = toReplace;
+
+    return origin;
+
+}
