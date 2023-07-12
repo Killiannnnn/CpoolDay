34b87735061af10cc8627c2b0f70c366413fbc3c
Killiannnnn <113891378+Killiannnnn@users.noreply.github.com>
2023-07-12 10:23:15 +0000

    [ADD] my_char_replace

---
+++ my_char_replace.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void print_char(char c);
+
+char* my_char_replace(char* origin, char toFind, char toReplace) {
+
+
+
+}
+
+int main(){
+
+    
+    char str[] = "bienvenue à epitech !";
+    printf("%s\n", my_char_replace(str, 'e', 'a'));
+
+    return 0;
+
+}
