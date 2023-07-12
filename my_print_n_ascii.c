7d5095bb3223f79c100c328e9ef34f7b94cf9b79
Killiannnnn <113891378+Killiannnnn@users.noreply.github.com>
2023-07-12 09:49:07 +0000

    [ADD] my_print_n_ascii

--- my_print_n_ascii.c
+++ my_print_n_ascii.c
@@ -4,7 +4,8 @@ void my_print_n_ascii(int howMany) {
 
     int i = 33;
 
-    for(int a = 0; a < howMany; a++, i++)
-        print_char(i);
+    if(howMany <= 94)
+        for(int a = 0; a < howMany; a++, i++)
+            print_char(i);
     
 }
